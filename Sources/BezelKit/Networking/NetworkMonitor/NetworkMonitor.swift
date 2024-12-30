06aa9420126c12168494a909dd5cb6b6708338ac
Daniel <danielprastiwa@gmail.com>
2024-12-30 10:44:19 +0700

    🏗️ Develop Netwok Monitor util

---
+++ Sources/BezelKit/Networking/NetworkMonitor/NetworkMonitor.swift
@@ -0,0 +1,25 @@
+//
+//  NetworkMonitor.swift
+//  BezelKit
+//
+//  Created by Daniel Prastiwa on 30/12/24.
+//
+
+import Foundation
+import Network
+import Combine
+
+
+public protocol NetworkMonitor {
+  var networkCondition: NetworkCondition { get }
+  func startNetworkMonitoring()
+  func stopNetworkMonitoring()
+}
+
+
+public protocol NetworkMonitorObserver {
+  var networkStatePublisher: PassthroughSubject<NetworkCondition, Never> { get }
+}
+
+
+public typealias NetworkMonitorImpl = NetworkMonitorImpl_V1
